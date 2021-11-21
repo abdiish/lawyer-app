e48fdb7431ae1e77873af68bd96a00a00d4d95df
Alan Cortes <ing.ti.upmh@gmail.com>
2021-11-20 20:14:07 -0600

    Componente Modal y servicio para mostrar información de cada expediente

---
+++ src/app/components/detalle-expediente/detalle-expediente.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Expediente } from '../../interfaces/cargar-expedientes';
+import { ExpedientesService } from '../../services/expedientes.service';
+
+@Component({
+  selector: 'app-detalle-expediente',
+  templateUrl: './detalle-expediente.component.html',
+  styleUrls: ['./detalle-expediente.component.scss'],
+})
+export class DetalleExpedienteComponent implements OnInit {
+
+  @Input() id: string;
+
+  judgment: Expediente =  {};
+
+  slideOpts = {
+    slidesPerView: 0.9,
+    freeMode: true
+  }
+
+  slideChips = {
+    slidesPerView: 0.77,
+    freeMode: true
+  }
+
+  constructor(private expedientesService: ExpedientesService) { }
+
+  ngOnInit() {
+
+    this.expedientesService.getExpediente(this.id).subscribe(resp => {
+      console.log(resp);
+      this.judgment = resp;
+    });
+  }
+
+}
---
+++ src/app/components/mapa/mapa.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+
+declare var mapboxgl: any;
+
+@Component({
+  selector: 'app-mapa',
+  templateUrl: './mapa.component.html',
+  styleUrls: ['./mapa.component.scss'],
+})
+export class MapaComponent implements OnInit {
+
+  @Input() coords: string;
+  @ViewChild('mapa', {static: true}) mapa: any;
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
